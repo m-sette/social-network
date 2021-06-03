3dc2947749be6742ba3756738e344df0bd34aeb0
Matteo <msette8@gmail.com>
2021-06-03 13:15:41 +0200

    Add read.me file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const csurf = require("csurf");
 //Socket io related
 const server = require("http").Server(app);
 const io = require("socket.io")(server, { origins: "localhost:8080" });
-//////////
+////////
 
 app.use(express.static("./public"));
 app.use(compression());
